10fa03e2ad58879a88734b57d1db2bb2510441b5
Aprat0135 <79624734+Aprat0135@users.noreply.github.com>
2021-03-08 08:54:14 +0100

    register finished not tested

--- inc/header.php
+++ inc/header.php
@@ -2,11 +2,11 @@
     <div>
         <?php
             if(isset($_SESSION['user'])) {
-                echo $_SESSION['user']." ";
+                echo $_SESSION['user'].' ';
                 echo '<a href="?add=logout">logout</a>';
             }
             else {
-                echo 'guest';
+                echo 'guest ';
                 echo '<a href="index.php">login</a>';
             }
             //logout
--- register.php
+++ register.php
@@ -14,7 +14,65 @@ session_start();
             include "inc/nav.html";
         ?>
         <div class="main">
-            <p>register</p>
+            <?php
+                // already logged in
+                if(isset($_SESSION['user'])) {
+                    echo "You are already logged in as " . $_SESSION['user'] . ".<br>";
+                }
+                else {
+                    // if user is set
+                    if(!empty($_POST['user']) {
+                        // if PWs are set
+                        if(!empty($_POST['pw']) {
+                            // hash PWs
+                            $pw = sha1($_POST['pw']);
+                            $pw2 = sha1($_POST['pw2']);
+
+                            // check pw
+                            if($pw == $pw2) {
+                                // Create connection
+                                $conn = new mysqli('localhost', 'root', '', 'logindb');
+                                // Check connection
+                                if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                                }
+
+                                // instead of $user = $_POST['user'];
+                                $user = $conn->real_escape_string(strip_tags(filter_input(INPUT_POST, "user")));
+
+                                $newUser = true;
+                                $result = $conn->query("SELECT username FROM userdata WHERE username = '$user'");
+                                if ($result) {
+                                    if ($row = $result->fetch_array()) {
+                                        echo "username already exists in database.<br>";
+                                        echo "<a href='register.php'>Register</a><br>";
+                                        $newUser = false;
+                                    }
+                                }
+
+                                if ($newUser) {
+                                    if ($conn->query("INSERT INTO userdata (username, password) VALUES ('$user', '$pw')")) {
+                                        echo "User $user has been registered.";
+                                    }
+                                }
+
+                                $conn->close();
+                            }
+                            else {
+                                echo "passwords have to be identical.<br>";
+                                echo "<a href='register.php'>Register</a><br>";
+                            }
+                        }
+                        else {
+                            echo "passwords musn't be empty.<br>";
+                            echo "<a href='register.php'>Register</a><br>";
+                        }
+                    }
+                    else {
+                        include 'inc/registerForm.html';
+                    }
+                }
+            ?>
         </div>
         <?php include "inc/footer.html";?>
     </body>
